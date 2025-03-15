89b5cf410c808e6858b7a4ba024458a51ea6511e
Broly 12 <brailynperezbatista@gmail.com>
2025-03-14 21:21:10 -0400

    ok

---
+++ ProductManager.API/Base/Validator/Validator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductManager.API.Base.Validator
+{
+    public static class Validator
+    {
+        public static bool isValidId(int id)
+        {
+            return id > 0;
+        }
+    }
+}
