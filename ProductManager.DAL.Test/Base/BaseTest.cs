dc0de6a92279cb6717a9b0925274458ab61c5c7b
Broly 12 <brailynperezbatista@gmail.com>
2025-03-17 19:28:52 -0400

    Pruebas unitarias completas almenos a DAL

---
+++ ProductManager.DAL.Test/Base/BaseTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using ProductManager.DAL.Test.DbInMemory;
+
+namespace ProductManager.DAL.Test.Base
+{
+    public class BaseTest<TRepository>
+    {
+        protected readonly ApplicationDbContext _context;
+        protected readonly Mock<ILogger<TRepository>> _mockLogger;
+
+        protected BaseTest()
+        {
+            _context = InMemoryDbService.GetProductManagerDbContext();
+            _mockLogger = new Mock<ILogger<TRepository>>();
+        }
+    }
+}
---
+++ ProductManager.DAL.Test/DbInMemory/InMemoryDbService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductManager.DAL.Test.DbInMemory
+{
+    public static class InMemoryDbService
+    {
+        public static ApplicationDbContext GetProductManagerDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new ApplicationDbContext(options);
+            context.Database.EnsureCreated();
+            return context;
+        }
+    }
+}
