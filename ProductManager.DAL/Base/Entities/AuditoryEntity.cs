d61f92bcfe60576f54a2c3c05fb8a0e5ae0d21bb
Broly 12 <brailynperezbatista@gmail.com>
2025-03-17 16:26:03 -0400

    Migraciones cambiadas a .DAL

---
+++ ProductManager.DAL/Base/Entities/AuditoryEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace ProductManager.DAL.Base.Entities
+{
+    public abstract class AuditoryEntity
+    {
+        protected AuditoryEntity()
+        {
+            CreatedAt = DateTime.Now;
+            Deleted = false;
+        }
+
+        public bool Deleted { get; set; }
+        public DateTime DeletedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
--- ProductManager.DAL/Entities/Category.cs
+++ ProductManager.DAL/Entities/Category.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using ProductManager.DAL.Base.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProductManager.DAL.Entities
 {
     [Table("Category")]
-    public class Category
+    public class Category : AuditoryEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
